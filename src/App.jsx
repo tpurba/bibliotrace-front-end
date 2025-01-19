a5db09bbac10130e04359a6ab226c2172ec47daa
Nikolas Earl <72423846+nearl2001@users.noreply.github.com>
2025-01-18 17:39:08 -0700

    feat(sitewide): add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import AppRoutes from "./routes/AppRoutes";
-// import './App.css'
+import AppRoutes from "./routes/AppRoutes"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,56 +8,7 @@ function App() {
     <div>
       <AppRoutes />
     </div>
-  //   <Router>
-  //   <div>
-  //     {/* Navigation Links (Optional) */}
-  //     <nav>
-  //       <Link to="/">Start Page</Link> | <Link to="/requestbook">Request Book Page</Link> | <Link to="/search">Search Page</Link>
-  //     </nav>
-
-  //     {/* Define Routes */}
-  //     <Routes>
-  //       <Route path="/" element={<StartPage />} />
-  //       <Route path="/requestbook" element={<RequestBook />} />
-  //       <Route path="/search" element={<SearchPage />} />
-  //     </Routes>
-  //   </div>
-  // </Router>
-  );
-};
-
-// function StartPage() {
-//   const navigate = useNavigate();
-
-//   const handleNavigate = () => {
-//     // Navigate programmatically
-//     navigate("/search");
-//   };
-
-//   return (
-//     <div>
-//       <h1>Start Page</h1>
-//       <button onClick={handleNavigate}>Go to Search Book</button>
-//     </div>
-//   );
-// }
-
-// function RequestBook() {
-//   return (
-//     <div>
-//       <h1>Request Book Page</h1>
-//       <p>Welcome! This is the Request Book Page.</p>
-//     </div>
-//   );
-// }
-
-// function SearchPage() {
-//   return (
-//     <div>
-//       <h1>Search Page</h1>
-//       <p>Welcome! This is the Search Page.</p>
-//     </div>
-//   );
-// }
+  )
+}
 
 export default App
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "tailwindcss"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    }
+  }
 })
