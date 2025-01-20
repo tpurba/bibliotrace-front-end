1207ebae910c7f7f360d66367bc314a3af7a5f93
Nikolas Earl <72423846+nearl2001@users.noreply.github.com>
2025-01-20 12:45:48 -0700

    feat(search): install pagination and image fetch logic with Google Books API

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 
 const SearchResult = ({ imageUrl, title, author, genre, series }) => {
+    const [image, setImage] = useState()
+
     const bookTitle = <p id='book-title'>{title}</p>
     const bookAuthor = <p id='book-author'>{author}</p>
     const bookGenre = <p id='book-genre'>{genre}</p>
     const bookSeries = <p id='book-series'>{series}</p>
 
+    useEffect(() => {
+        const fetchImage = async () => {
+            if (imageUrl === 'Unknown') {
+                return
+            }
+
+            try {
+                const response = await fetch('https://cors-anywhere.herokuapp.com/' + imageUrl)
+                if (response.ok) {
+                    const blob = await response.blob()
+                    const objectURL = URL.createObjectURL(blob)
+                    console.log(objectURL)
+                    setImage(objectURL)
+                }
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        fetchImage()
+    }, []) // Effect only runs once during component rendering
+
     return (
         <div className="flex h-fit border-x-2 border-x-[#110057] border-b-2 border-b-[#110057] bg-[#F7F5F9]">
             <div className="h-24 flex items-center justify-center px-3 border-r-slate-50 border-r-2 w-20 text-transparent">
-                <img src='/src/assets/harry-potter.jpg' className="w-16 m-2"></img>
+                {(imageUrl != 'Unknown') ? <img src={image} className="w-16 m-2" alt="Cover Thumbnail"></img> : null}
             </div>
             <div className="flex items-center justify-center px-3 border-r-slate-50 border-r-2 w-1/3">
                 {bookTitle}
@@ -26,8 +49,6 @@ const SearchResult = ({ imageUrl, title, author, genre, series }) => {
             </div>
         </div>
     )
-
-
 }
 
 export default SearchResult
