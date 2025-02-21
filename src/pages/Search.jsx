e1dbe7bfcecd97786a3431a96d4a8bcc6e3b18cd
kennion <kennion@byu.edu>
2025-02-21 02:09:33 -0700

    Rewire endpoints to new response objects

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,127 +1,133 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import Cookies from 'js-cookie';
-import NavBar from "../components/NavBar.jsx"
-import Filter from "../assets/filter.svg?react"
-import Next from "../assets/next.svg?react"
-import Prev from "../assets/prev.svg?react"
-import SearchResult from "../components/SearchResult.jsx"
+import Cookies from "js-cookie";
+import NavBar from "../components/NavBar.jsx";
+import Filter from "../assets/filter.svg?react";
+import Next from "../assets/next.svg?react";
+import Prev from "../assets/prev.svg?react";
+import SearchResult from "../components/SearchResult.jsx";
 import LoadingSpinner from "../components/LoadingSpinner.jsx";
 import FilterBox from "../components/FilterBox.jsx";
 const Search = () => {
-  const location = useLocation()
-  const rowHeight = 160
-  const headerHeight = 450
+  const location = useLocation();
+  const rowHeight = 160;
+  const headerHeight = 450;
   const [showFilter, setShowFilter] = useState(false);
-  const [rowCount, setRowCount] = useState(0)
-  const [pageOffset, setPageOffset] = useState(0)
-  const [isLoading, setLoading] = useState(false)
-  const [searchInput, setSearchInput] = useState(location.state?.initSearchInput ?? '') // Arch idea: use the initSearchInput as a text-based passthrough for searches by Genre, Age group, date range, etc...
-  // Maybe the string can be like "||GENRE:fantasy||" or "||AGE:board||" or "||DATE_START:01/02/0003 DATE_END:04/05/0006||" or "||IDENTIFIER:searchstring||" 
-  const [filterInput, setFilterInput] = useState(location.state?.initFilterInput ?? { Audiences: [], Genres: [] })
-  const [inputQuery, setInputQuery] = useState('')
-  const [searchResults, setSearchResults] = useState([{
-    key: 'helloSearch',
-    selfLink: 'none',
-    title: '',
-    author: '',
-    isbn: 'none',
-    genre: '',
-    series: ''
-  }])
+  const [rowCount, setRowCount] = useState(0);
+  const [pageOffset, setPageOffset] = useState(0);
+  const [isLoading, setLoading] = useState(false);
+  const [searchInput, setSearchInput] = useState(location.state?.initSearchInput ?? ""); // Arch idea: use the initSearchInput as a text-based passthrough for searches by Genre, Age group, date range, etc...
+  // Maybe the string can be like "||GENRE:fantasy||" or "||AGE:board||" or "||DATE_START:01/02/0003 DATE_END:04/05/0006||" or "||IDENTIFIER:searchstring||"
+  const [filterInput, setFilterInput] = useState(
+    location.state?.initFilterInput ?? { Audiences: [], Genres: [] }
+  );
+  const [inputQuery, setInputQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([
+    {
+      key: "helloSearch",
+      selfLink: "none",
+      title: "",
+      author: "",
+      isbn: "none",
+      genre: "",
+      series: "",
+    },
+  ]);
 
   const conductSearch = () => {
-    if ((searchInput == '' || searchInput == null) && (filterInput == '' || filterInput == null)) {
-      return
+    if (
+      (searchInput == "" || searchInput == null) &&
+      (filterInput == "" || filterInput == null)
+    ) {
+      return;
     }
-    setLoading(true)
+    setLoading(true);
 
     // Generate Filter String
-    let filterString = ""
+    let filterString = "";
     if (filterInput.Audiences.length > 0) {
-      filterString += '||Audience:'
+      filterString += "||Audience:";
       for (let i = 0; i < filterInput.Audiences.length; i++) {
-        filterString += filterInput.Audiences[i]
+        filterString += filterInput.Audiences[i];
         if (i < filterInput.Audiences.length - 1) {
-          filterString += ','
+          filterString += ",";
         }
       }
-      filterString += "||"
+      filterString += "||";
     }
     if (filterInput.Genres.length > 0) {
-      filterString += '||Genre:'
+      filterString += "||Genre:";
       for (let i = 0; i < filterInput.Genres.length; i++) {
-        filterString += filterInput.Genres[i]
+        filterString += filterInput.Genres[i];
         if (i < filterInput.Genres.length - 1) {
-          filterString += ','
+          filterString += ",";
         }
       }
-      filterString += "||"
+      filterString += "||";
     }
 
     try {
-      const jwt = Cookies.get('authToken')
+      const jwt = Cookies.get("authToken");
       fetch(`http://localhost:8080/api/search/query/${filterString}${searchInput}`, {
         headers: {
-          "Authorization": `Bearer ${jwt}`
-        }
+          Authorization: `Bearer ${jwt}`,
+        },
       })
         .then((response) => {
           if (response.status === 200) {
             response.json().then((data) => {
-              const bookItems = data.results
-              console.log('I GOT THE RESULTS!!')
-              console.log(bookItems)
-              setSearchResults(bookItems)
-              setPageOffset(0)
-              setInputQuery(searchInput)
-              setLoading(false)
-            })
+              const bookItems = data.results;
+              console.log("I GOT THE RESULTS!!");
+              console.log(bookItems);
+              setSearchResults(bookItems);
+              setPageOffset(0);
+              setInputQuery(searchInput);
+              setLoading(false);
+            });
           } else {
             setSearchResults([
               {
                 author: `Error: ${response.status} Code Received`,
-                key: 'Error Code'
+                key: "Error Code",
               },
               {
                 author: `Error Address: ${response.url}`,
-                key: 'Error Addy'
-              }
-            ])
-            setPageOffset(0)
-            setInputQuery(searchInput)
-            setLoading(false)
+                key: "Error Addy",
+              },
+            ]);
+            setPageOffset(0);
+            setInputQuery(searchInput);
+            setLoading(false);
           }
-
         })
         .catch((error) => {
           setSearchResults([
             {
               author: `Error: ${error}`,
-              key: 'errorResult'
-            }
-          ])
-          setPageOffset(0)
-          setInputQuery(searchInput)
-          setLoading(false)
-        })
+              key: "errorResult",
+            },
+          ]);
+          setPageOffset(0);
+          setInputQuery(searchInput);
+          setLoading(false);
+        });
     } catch (error) {
       setSearchResults([
         {
-          author: `Error: ${error}`
-        }
-      ])
-      setPageOffset(0)
-      setInputQuery(searchInput)
-      setLoading(false)
+          author: `Error: ${error}`,
+        },
+      ]);
+      setPageOffset(0);
+      setInputQuery(searchInput);
+      setLoading(false);
     }
-  }
+  };
 
   const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      conductSearch()
+    if (event.key === "Enter") {
+      conductSearch();
     }
-  }
+  };
 
   // Function to calculate the number of rows that can fit
   const calculateRowCount = () => {
@@ -132,78 +138,109 @@ const Search = () => {
 
   // Effect that runs once at initialization
   useEffect(() => {
-    if (searchInput != '') {
-      conductSearch()
+    if (searchInput != "") {
+      conductSearch();
     }
 
     calculateRowCount(); // Initial calculation
-    window.addEventListener('resize', calculateRowCount);
+    window.addEventListener("resize", calculateRowCount);
 
     // Clean up the listener on unmount
     return () => {
-      window.removeEventListener('resize', calculateRowCount);
+      window.removeEventListener("resize", calculateRowCount);
     };
   }, []);
 
   useEffect(() => {
-    if (searchInput != '' || filterInput.Audiences.length > 0 || filterInput.Genres.length > 0) {
-      conductSearch()
+    if (
+      searchInput != "" ||
+      filterInput.Audiences.length > 0 ||
+      filterInput.Genres.length > 0
+    ) {
+      conductSearch();
     }
-  }, [filterInput])
+  }, [filterInput]);
 
   const incrementPage = () => {
-    if ((pageOffset + rowCount) < searchResults.length - 1) {
-      setPageOffset(pageOffset + rowCount)
+    if (pageOffset + rowCount < searchResults.length - 1) {
+      setPageOffset(pageOffset + rowCount);
     }
-  }
+  };
 
   const decrementPage = () => {
     if (pageOffset - rowCount < 0) {
-      setPageOffset(0)
+      setPageOffset(0);
     } else {
-      setPageOffset(pageOffset - rowCount)
+      setPageOffset(pageOffset - rowCount);
     }
-  }
+  };
 
   const handleFilterPress = () => {
     console.log("Buttons been pressed");
-    setShowFilter(!showFilter)
+    setShowFilter(!showFilter);
   };
 
   return (
-    <div className="w-screen pb-5 search-bg flex flex-col items-center" style={{ minHeight: "100vh" }}>
-      <NavBar useDarkTheme={true} showTitle={false} bgColor={'none'} />
+    <div
+      className="w-screen pb-5 search-bg flex flex-col items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <NavBar useDarkTheme={true} showTitle={false} bgColor={"none"} />
       <h1 className="mt-16 text-5xl">Bibliotrace 3.0</h1>
-      <div className="h-16 my-6 flex w-7/12 justify-center"> {/* Search Bar */}
-        <input className="m-2 px-3 w-10/12 border-2 border-[#110057] rounded-2xl" type="text" placeholder="Search For Books" value={searchInput} onInput={e => setSearchInput(e.target.value)} onKeyDown={handleKeyDown}></input>
-        <button className="flex items-center m-2 border-[#110057] border-2 bg-white rounded-2xl" onClick={conductSearch}>
-          {isLoading ? <LoadingSpinner size={'3rem'} /> : "Go!"}
+      <div className="h-16 my-6 flex w-7/12 justify-center">
+        {" "}
+        {/* Search Bar */}
+        <input
+          className="m-2 px-3 w-10/12 border-2 border-[#110057] rounded-2xl"
+          type="text"
+          placeholder="Search For Books"
+          value={searchInput}
+          onInput={(e) => setSearchInput(e.target.value)}
+          onKeyDown={handleKeyDown}
+        ></input>
+        <button
+          className="flex items-center m-2 border-[#110057] border-2 bg-white rounded-2xl"
+          onClick={conductSearch}
+        >
+          {isLoading ? <LoadingSpinner size={"3rem"} /> : "Go!"}
         </button>
       </div>
-      <div className="w-10/12"> {/* Search Results Table */}
-        <div className="w-full flex justify-between my-4"> {/* Buttons Above Results */}
+      <div className="w-10/12">
+        {" "}
+        {/* Search Results Table */}
+        <div className="w-full flex justify-between my-4">
+          {" "}
+          {/* Buttons Above Results */}
           <button className="bg-transparent border-none " onClick={handleFilterPress}>
             <Filter />
           </button>
           {/* Filter Box (Overlay) */}
           {showFilter && (
-            <div className="fixed top-5 left-52 w-full h-full flex justify-center items-center z-50
-                          sm:w-80 md:w-96 lg:w-[30vw] xl:w-[50vw] max-w-full p-4">
-              <FilterBox 
+            <div
+              className="fixed top-5 left-52 w-full h-full flex justify-center items-center z-50
+                          sm:w-80 md:w-96 lg:w-[30vw] xl:w-[50vw] max-w-full p-4"
+            >
+              <FilterBox
                 onClose={(selectedFilters) => {
                   setShowFilter(false);
                   setFilterInput(selectedFilters);
-                }} 
+                }}
                 prevSelectedItems={filterInput}
               />
             </div>
           )}
-          <p className="flex items-center">{(inputQuery != '') ? `Showing search results for "${inputQuery}"` : ""}</p>
-          <div className="w-10"></div> {/* placeholder to put above p element in the center */}
+          <p className="flex items-center">
+            {inputQuery != "" ? `Showing search results for "${inputQuery}"` : ""}
+          </p>
+          <div className="w-10"></div>{" "}
+          {/* placeholder to put above p element in the center */}
         </div>
-        
-        <div className=""> {/* Results Table */}
-          <div className="h-10 flex justify-between bg-[#110057] text-white text-center items-center rounded-t-2xl"> {/* Table Header */}
+        <div className="">
+          {" "}
+          {/* Results Table */}
+          <div className="h-10 flex justify-between bg-[#110057] text-white text-center items-center rounded-t-2xl">
+            {" "}
+            {/* Table Header */}
             <div className="h-10 flex items-center justify-center px-3 border-r-slate-50 border-r-2 w-28 text-transparent">
               <h3>Cover</h3>
             </div>
@@ -221,11 +258,11 @@ const Search = () => {
             </div>
           </div>
           {Array.from({ length: rowCount }, (_, index) => {
-            const targetIndex = index + pageOffset
+            const targetIndex = index + pageOffset;
             if (targetIndex >= searchResults.length) {
-              return null
+              return null;
             }
-            const bookData = searchResults[index + pageOffset]
+            const bookData = searchResults[index + pageOffset];
             return bookData ? (
               <SearchResult
                 key={bookData.id}
@@ -234,24 +271,37 @@ const Search = () => {
                 title={bookData.title}
                 author={bookData.author}
                 genre={bookData.genre}
-                series={bookData.series} />
-            ) : null
+                series={bookData.series}
+              />
+            ) : null;
           })}
         </div>
-        <div className="flex align-middle items-center justify-center py-4"> {/* Pagination Buttons */}
-          <button className="bg-transparent flex flex-col items-center" onClick={decrementPage}>
+        <div className="flex align-middle items-center justify-center py-4">
+          {" "}
+          {/* Pagination Buttons */}
+          <button
+            className="bg-transparent flex flex-col items-center"
+            onClick={decrementPage}
+          >
             <Prev />
             <p className="">Previous</p>
           </button>
-          <p className="h-12">Showing {pageOffset + 1}-{Math.min(pageOffset + rowCount, searchResults.length)} of {searchResults.length}</p>
-          <button className="bg-transparent flex flex-col items-center" onClick={incrementPage}>
+          <p className="h-12">
+            Showing {pageOffset + 1}-
+            {Math.min(pageOffset + rowCount, searchResults.length)} of{" "}
+            {searchResults.length}
+          </p>
+          <button
+            className="bg-transparent flex flex-col items-center"
+            onClick={incrementPage}
+          >
             <Next />
             <p className="">Next</p>
           </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Search;
