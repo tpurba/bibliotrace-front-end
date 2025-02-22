83853bcd98cd90fb275af06c4f452a45b4bb597d
Nikolas Earl <72423846+nearl2001@users.noreply.github.com>
2025-02-22 16:50:14 -0700

    feat(bookdata modal): first draft of the book information modal

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,77 +1,122 @@
-import React, { useState, useEffect } from "react"
-import defaultBook from '../assets/generic-book.png'
-import Cookies from 'js-cookie'
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import defaultBook from "../assets/generic-book.png";
+import BookDetails from "../modals/BookDetails";
+import Cookies from "js-cookie";
 
-const SearchResult = ({ isbn, coverImageId, title, author, genre, series }) => {
-    const [image, setImage] = useState(defaultBook)
+const SearchResult = ({ bookData }) => {
+  const [image, setImage] = useState(defaultBook);
+  const [openModal, setOpenModal] = useState(false);
 
-    const bookTitle = <p id='book-title' className="text-center">{title}</p>
-    const bookAuthor = <p id='book-author' className="text-center">{author}</p>
-    const bookGenre = <p id='book-genre' className="text-center">{genre}</p>
-    const bookSeries = <p id='book-series' className="text-center">{series}</p>
+  const navigate = useNavigate(); 
 
-    useEffect(() => {
-        const fetchImage = async () => {
-            if (isbn == null) {
-                return
+  const isbn = bookData.isbn;
+  const coverImageId = bookData.coverImageId;
+  const title = bookData.title;
+  const author = bookData.author;
+  const genre = bookData.genre;
+  const series = bookData.series;
+
+  const bookTitle = (
+    <p id="book-title" className="text-center">
+      {title}
+    </p>
+  );
+  const bookAuthor = (
+    <p id="book-author" className="text-center">
+      {author}
+    </p>
+  );
+  const bookGenre = (
+    <p id="book-genre" className="text-center">
+      {genre}
+    </p>
+  );
+  const bookSeries = (
+    <p id="book-series" className="text-center">
+      {series}
+    </p>
+  );
+
+  const toggleModal = () => {
+    setOpenModal((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      if (isbn == null) {
+        return;
+      }
+
+      const jwt = Cookies.get("authToken");
+
+      try {
+        let response;
+        if (coverImageId != null && coverImageId != "") {
+          response = await fetch(
+            `http://localhost:8080/api/search/cover/${coverImageId}`,
+            {
+              method: "GET",
+              headers: {
+                Authorization: `Bearer ${jwt}`,
+              },
             }
-
-            const jwt = Cookies.get('authToken')
-
-            try {
-                let response
-                if (coverImageId != null && coverImageId != '') {
-                    response = await fetch(`http://localhost:8080/api/search/cover/${coverImageId}`, {
-                        method: 'GET', 
-                        headers: {
-                            Authorization: `Bearer ${jwt}`
-                        }
-                    })
-                } else {
-                    response = await fetch(`http://localhost:8080/api/search/cover/${isbn}`, {
-                        method: 'GET', 
-                        headers: {
-                            Authorization: `Bearer ${jwt}`
-                        }
-                    })
-                }
-                
-                if (response.ok) {
-                    const blob = await response.blob()
-                    if (blob.size >= 100) {
-                        const objectURL = URL.createObjectURL(blob)
-                        setImage(objectURL)
-                    }
-                } else {
-                    setImage(defaultBook)
-                }
-            } catch (e) {
-                console.log(e)
-                setImage(defaultBook)
-            }
+          );
+        } else {
+          response = await fetch(`http://localhost:8080/api/search/cover/${isbn}`, {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${jwt}`,
+            },
+          });
         }
-        fetchImage()
-    }, [isbn]) 
 
-    return (
-        <div className="flex justify-between h-fit border-x-2 border-x-[#110057] border-b-2 border-b-[#110057] bg-[#FFFFFF] bg-opacity-90 text-xl">
-            <div className="h-40 flex items-center justify-center px-3 border-r-slate-50 border-r-2 w-28 text-transparent">
-                <img src={image} className="w-28 m-2" alt="Cover Thumbnail"></img>
-            </div>
-            <div className="flex items-center justify-center px-3 border-r-slate-50 border-r-2 w-1/3">
-                {bookTitle}
-            </div>
-            <div className="flex items-center justify-center px-3 border-r-slate-50 border-r-2 w-1/5">
-                {bookAuthor}
-            </div>
-            <div className="flex items-center justify-center px-3 border-r-slate-50 border-r-2 w-1/5">
-                {bookGenre}
-            </div>
-            <div className="flex items-center justify-center px-3 w-1/5">
-                {bookSeries}
-            </div>
+        if (response.ok) {
+          const blob = await response.blob();
+          if (blob.size >= 100) {
+            const objectURL = URL.createObjectURL(blob);
+            setImage(objectURL);
+          }
+        } else {
+            if (response.status === 401) {
+                navigate('/login')
+            }
+            setImage(defaultBook);
+        }
+      } catch (e) {
+        console.log(e);
+        setImage(defaultBook);
+      }
+    };
+    fetchImage();
+  }, [isbn]);
+
+  return (
+    <div>
+      <div
+        className="flex justify-between h-fit border-x-2 border-x-[#110057] border-b-2 border-b-[#110057] bg-[#FFFFFF] bg-opacity-90 text-xl"
+        onClick={toggleModal}
+      >
+        <div className="h-40 flex items-center justify-center px-3 border-r-slate-50 border-r-2 w-28 text-transparent">
+          <img src={image} className="w-28 m-2" alt="Cover Thumbnail"></img>
         </div>
-    )
-}
+        <div className="flex items-center justify-center px-3 border-r-slate-50 border-r-2 w-1/3">
+          {bookTitle}
+        </div>
+        <div className="flex items-center justify-center px-3 border-r-slate-50 border-r-2 w-1/5">
+          {bookAuthor}
+        </div>
+        <div className="flex items-center justify-center px-3 border-r-slate-50 border-r-2 w-1/5">
+          {bookGenre}
+        </div>
+        <div className="flex items-center justify-center px-3 w-1/5">{bookSeries}</div>
+      </div>
 
-export default SearchResult
+      {openModal && (
+        <BookDetails onExit={toggleModal} bookData={bookData} imageSrc={image} />
+      )}
+    </div>
+  );
+};
+
+export default SearchResult;
