6389a749c1c19875e88f196850a03606f0e0d744
Nikolas Earl <72423846+nearl2001@users.noreply.github.com>
2025-03-07 11:22:29 -0700

    fix(adminhome): set genre and ages using a state hook instead of on component import

--- src/components/admin/AdminMainMenu.jsx
+++ src/components/admin/AdminMainMenu.jsx
@@ -1,4 +1,4 @@
-//Icons 
+//Icons
 import OrangeIcon from "../../assets/ExploreByAge.jpg";
 import BlueIcon from "../../assets/ExploreByGenre.jpg";
 import YellowIcon from "../../assets/NewArrivalsIcon.jpg";
@@ -6,89 +6,137 @@ import RedIcon from "../../assets/SuggestBook.jpg";
 import PinkIcon from "../../assets/WhatsPopular.jpg";
 import PurpleIcon from "../../assets/checkoutIcon.jpg";
 import PeachColorIcon from "../../assets/CheckInIcon.jpg";
-//Libraries 
+//Libraries
 import { useNavigate } from "react-router-dom";
 import CustomButton from "../ButtonComponent";
-import React, { useState} from "react"
+import React, { useState } from "react";
 import PopUpBar from "../../modals/PopUpSideBar";
-import Cookies from 'js-cookie';
-
-const genreListString = Cookies.get('genreList')
-let genres = []
-if (genreListString) {
-  const genreList = genreListString.split(',')
-  genres = genreList.map((genre) => {return {text: genre}})
-}
-
-
-const agesListString = Cookies.get('audienceList')
-let ages= []
-if (agesListString) {
-  const agesList = agesListString.split(",")
-  ages = agesList.map((age) => {return {text: age}})
-}
+import Cookies from "js-cookie";
 
 export default function MainMenu() {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const [isBlurred, setIsBlurred] = useState(false);
   const [showPopupBarAge, setShowPopupBarAge] = useState(false);
   const [showPopupBarGenre, setShowPopupBarGenre] = useState(false);
+  const [ages, setAges] = useState([]);
+  const [genres, setGenres] = useState([]);
 
   const handleTestClick = () => {
-    console.log('Button pressed');
+    console.log("Button pressed");
   };
-  
+
   const handleCheckout = () => {
     navigate("/checkout");
-  }
+  };
 
   const handleCheckIn = () => {
     navigate("/checkin");
-  }
+  };
 
   const handleSuggestBookNav = () => {
     navigate("/suggest");
   };
 
   const handleExploreByAge = () => {
-    setShowPopupBarAge(!showPopupBarAge)
+    setShowPopupBarAge(!showPopupBarAge);
   };
 
   const handleExploreByGenre = () => {
-    setShowPopupBarGenre(!showPopupBarGenre)
+    setShowPopupBarGenre(!showPopupBarGenre);
   };
-  
+
   const handlePopular = (filterInput) => {
-    navigate("/search", { state: { initFilterInput: { Audiences: [], Genres: [], Special: ["Popular"] } }});
+    navigate("/search", {
+      state: { initFilterInput: { Audiences: [], Genres: [], Special: ["Popular"] } },
+    });
   };
 
   const handleNewest = (filterInput) => {
-    navigate("/search", { state: { initFilterInput: { Audiences: [], Genres: [], Special: ["Newest"] } }});
+    navigate("/search", {
+      state: { initFilterInput: { Audiences: [], Genres: [], Special: ["Newest"] } },
+    });
   };
 
   const menuItems = [
-    { label: 'Check In', imageSrc: PeachColorIcon, onclick: handleCheckIn, borderColor: "#fbb7a4", textWidth: '35rem'},
-    { label: 'Suggest A Book', imageSrc: RedIcon, onclick: handleSuggestBookNav, borderColor: "#e12502", textWidth: '35rem'},
-    { label: 'Popular', imageSrc: PinkIcon, onclick: handlePopular, borderColor: "#ff50df", textWidth: '35rem'},
-    { label: 'New Arrivals', imageSrc: YellowIcon, onclick: handleNewest, borderColor: "#FFD700", textWidth: '35rem'},
-    { label: 'By Age', imageSrc: OrangeIcon, onclick: handleExploreByAge, borderColor: "#fa8804", textWidth: '35rem'},
-    { label: 'By Genre', imageSrc: BlueIcon, onclick: handleExploreByGenre, borderColor: "#669bff", textWidth: '35rem'},
+    {
+      label: "Check In",
+      imageSrc: PeachColorIcon,
+      onclick: handleCheckIn,
+      borderColor: "#fbb7a4",
+      textWidth: "35rem",
+    },
+    {
+      label: "Suggest A Book",
+      imageSrc: RedIcon,
+      onclick: handleSuggestBookNav,
+      borderColor: "#e12502",
+      textWidth: "35rem",
+    },
+    {
+      label: "Popular",
+      imageSrc: PinkIcon,
+      onclick: handlePopular,
+      borderColor: "#ff50df",
+      textWidth: "35rem",
+    },
+    {
+      label: "New Arrivals",
+      imageSrc: YellowIcon,
+      onclick: handleNewest,
+      borderColor: "#FFD700",
+      textWidth: "35rem",
+    },
+    {
+      label: "By Age",
+      imageSrc: OrangeIcon,
+      onclick: handleExploreByAge,
+      borderColor: "#fa8804",
+      textWidth: "35rem",
+    },
+    {
+      label: "By Genre",
+      imageSrc: BlueIcon,
+      onclick: handleExploreByGenre,
+      borderColor: "#669bff",
+      textWidth: "35rem",
+    },
   ];
 
-    
+  useEffect(() => {
+    const genreListString = Cookies.get("genreList");
+    if (genreListString) {
+      const genreList = genreListString.split(",");
+      setGenres((oldList) => {
+        return genreList.map((genre) => {
+          return { text: genre };
+        });
+      });
+    }
+  
+    const agesListString = Cookies.get("audienceList");
+    if (agesListString) {
+      const agesList = agesListString.split(",");
+      setAges((oldList) => {
+        return agesList.map((age) => {
+          return { text: age };
+        });
+      });
+    }
+  }, [])
+
   return (
     <div className="absolute left-0 top-auto z-10">
       <CustomButton
-          imageSrc={PurpleIcon}
-          text="CHECK OUT"
-          textColor="#FFFFFF" //same 
-          onClick={handleCheckout}
-          borderColor="#4b00e3" 
-          bgColor="#110057"//same
-          layout='row'//same
-          textWidth= '42vw'
-          textSize="1.25rem"
-    />
+        imageSrc={PurpleIcon}
+        text="CHECK OUT"
+        textColor="#FFFFFF" //same
+        onClick={handleCheckout}
+        borderColor="#4b00e3"
+        bgColor="#110057" //same
+        layout="row" //same
+        textWidth="42vw"
+        textSize="1.25rem"
+      />
       <div className="grid grid-cols-2 w-2/3 justify-start">
         {menuItems.map((item, index) => (
           <CustomButton
@@ -97,9 +145,9 @@ export default function MainMenu() {
             text={item.label}
             textColor="#FFFFFF"
             onClick={item.onclick}
-            borderColor={item.borderColor} 
+            borderColor={item.borderColor}
             bgColor="#110057"
-            layout='row'
+            layout="row"
             textWidth={item.textWidth}
             textSize="1.25rem"
           />
@@ -107,28 +155,27 @@ export default function MainMenu() {
       </div>
       {/* Conditionally Render PullOutBarGenre */}
       {showPopupBarAge && (
-          <PopUpBar 
-            onClose={() => setShowPopupBarAge(false)} 
-            buttons={ages} 
-            side={"right"}
-            uniformColor={"#fa8804"}
-            titleText={"Explore By Age"}    
-            buttonWidth={'14vw'}
-            buttonHeight={"10vh"}      
-          />
-        )}
+        <PopUpBar
+          onClose={() => setShowPopupBarAge(false)}
+          buttons={ages}
+          side={"right"}
+          uniformColor={"#fa8804"}
+          titleText={"Explore By Age"}
+          buttonWidth={"14vw"}
+          buttonHeight={"10vh"}
+        />
+      )}
       {showPopupBarGenre && (
-          <PopUpBar 
-            onClose={() => setShowPopupBarGenre(false)} 
-            buttons={genres} 
-            side={"right"}
-            uniformColor={"#669bff"}
-            titleText={"Explore By Genre"}
-            buttonWidth={'14vw'}
-            buttonHeight={"8vh"}
-          />
-        )}
+        <PopUpBar
+          onClose={() => setShowPopupBarGenre(false)}
+          buttons={genres}
+          side={"right"}
+          uniformColor={"#669bff"}
+          titleText={"Explore By Genre"}
+          buttonWidth={"14vw"}
+          buttonHeight={"8vh"}
+        />
+      )}
     </div>
-    
   );
 }
