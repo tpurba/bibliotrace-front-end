5fe15b1f34b77da1c14fd5b61b45b52e9241bd90
Kelly Ko <kellyko2003@gmail.com>
2025-03-25 17:16:21 -0600

    update styles and fetch calls

--- src/pages/ManageGenresTags.jsx
+++ src/pages/ManageGenresTags.jsx
@@ -3,21 +3,26 @@ import Cookies from "js-cookie";
 import NavBar from "../components/NavBar";
 
 export default function ManageGenresTags() {
-  //TODO: SET THESE AS REACT VARIABLES INSTEAD OF STORING THEM IN COOKIES
-  const [genres, setGenres] = useState(["Adventure", "Mystery"]);
-  const [tags, setTags] = useState(["Picture Book", "YA", "New"]);
-  const [genreToAdd, setGenreToAdd] = useState("");
-  const [tagToAdd, setTagToAdd] = useState("");
+  const [genres, setGenres] = useState([]);
+  const [tags, setTags] = useState([]);
 
   useEffect(() => {
-    // setGenres();
-    // setTags();
+    let genreList = Cookies.get("genreList");
+    setGenres(genreList.split(","));
   }, []);
 
-  async function handleAddGenre() {
+  async function handleAddGenre(e) {
     //fetch call to add genreToAdd
+    e.preventDefault();
     try {
-      const response = fetch("https://localhost:8080/api/inventory/genre", {
+      const formData = new FormData(e.target);
+      const genreToAdd = formData.get("genre_name");
+
+      if (!genreToAdd) {
+        return;
+      }
+
+      const response = await fetch("http://localhost:8080/api/inventory/genre", {
         method: "POST",
         headers: { "Content-Type": "application/json", Authorization: `Bearer ${Cookies.get("authToken")}` },
         body: JSON.stringify({ genre_name: genreToAdd }),
@@ -26,6 +31,8 @@ export default function ManageGenresTags() {
       const data = await response.json();
       if (!response.ok) {
         console.log(data.message);
+      } else {
+        setGenres([...genres, genreToAdd].sort());
       }
     } catch (e) {
       console.log(e);
@@ -35,7 +42,7 @@ export default function ManageGenresTags() {
   async function handleRemoveGenre(genre) {
     //fetch call to remove genre
     try {
-      const response = fetch("https://localhost:8080/api/inventory/genre", {
+      const response = await fetch("http://localhost:8080/api/inventory/genre", {
         method: "DELETE",
         headers: { "Content-Type": "application/json", Authorization: `Bearer ${Cookies.get("authToken")}` },
         body: JSON.stringify({ genre_name: genre }),
@@ -44,16 +51,26 @@ export default function ManageGenresTags() {
       const data = await response.json();
       if (!response.ok) {
         console.log(data.message);
+      } else {
+        setGenres(genres.filter((g) => g !== genre));
       }
     } catch (e) {
       console.log(e);
     }
   }
 
-  async function handleAddTag() {
+  async function handleAddTag(e) {
     //fetch call to add tagToAdd
+    e.preventDefault();
     try {
-      const response = fetch("https://localhost:8080/api/inventory/tag", {
+      const formData = new FormData(e.target);
+      const tagToAdd = formData.get("tag_name");
+
+      if (!tagToAdd) {
+        return;
+      }
+
+      const response = await fetch("http://localhost:8080/api/inventory/tag", {
         method: "POST",
         headers: { "Content-Type": "application/json", Authorization: `Bearer ${Cookies.get("authToken")}` },
         body: JSON.stringify({ tag_name: tagToAdd }),
@@ -62,6 +79,8 @@ export default function ManageGenresTags() {
       const data = await response.json();
       if (!response.ok) {
         console.log(data.message);
+      } else {
+        setTags([...tags, tagToAdd].sort());
       }
     } catch (e) {
       console.log(e);
@@ -71,7 +90,7 @@ export default function ManageGenresTags() {
   async function handleRemoveTag(tag) {
     //fetch call to remove tag
     try {
-      const response = fetch("https://localhost:8080/api/inventory/tag", {
+      const response = await fetch("http://localhost:8080/api/inventory/tag", {
         method: "DELETE",
         headers: { "Content-Type": "application/json", Authorization: `Bearer ${Cookies.get("authToken")}` },
         body: JSON.stringify({ tag_name: tag }),
@@ -80,6 +99,8 @@ export default function ManageGenresTags() {
       const data = await response.json();
       if (!response.ok) {
         console.log(data.message);
+      } else {
+        setTags(tags.filter((t) => t !== tag));
       }
     } catch (e) {
       console.log(e);
@@ -89,47 +110,51 @@ export default function ManageGenresTags() {
   return (
     <>
       <NavBar useDarkTheme={true} showTitle={true} showNavButtons={true}></NavBar>
-      <h1>Edit Genres/Tags</h1>
-      <div className="flex flex-row space-around">
-        <section id="genres-container" className="border">
-          <ul>
+      <h1 className="text-center my-10">Edit Genres/Tags</h1>
+      <div className="flex flex-row justify-between w-full h-[100vh]">
+        <section id="genres-container" className="flex-1 w-full h-full mx-[5%] ml-[10%] ">
+          <ul className="border h-[60%] overflow-y-scroll">
             {genres.map((genre) => {
               return (
-                <li className="flex flex-row flex-nowrap">
-                  <p>{genre}</p>
-                  <button onClick={(genre) => handleRemoveGenre(genre)}>Remove</button>
-                </li>
+                <>
+                  <li key={genre} className="flex flex-row flex-nowrap justify-between my-5 mx-10 text-lg items-center">
+                    <p>{genre}</p>
+                    <button onClick={() => handleRemoveGenre(genre)}>Remove</button>
+                  </li>
+                  <hr key={"hr" + genre} className="mx-10 opacity-30" />
+                </>
               );
             })}
           </ul>
-
-          <input
-            type="text"
-            placeholder="Genre Name"
-            name="genre-name"
-            onChange={(e) => setGenreToAdd(e.target.value)}
-          ></input>
-          <button onClick={handleAddGenre}>Add Genre</button>
+          <form className="flex flex-row w-full my-3" onSubmit={(e) => handleAddGenre(e)}>
+            <input
+              className="min-w-0 border grow mr-3 p-2"
+              type="text"
+              placeholder="Genre Name"
+              name="genre_name"
+            ></input>
+            <button type="submit">Add Genre</button>
+          </form>
         </section>
 
-        <section id="tags-container" className="border">
-          <ul className="">
+        <section id="tags-container" className="flex-1 w-full h-full mx-[5%] mr-[10%]">
+          <ul className="border h-[60%] overflow-y-scroll">
             {tags.map((tag) => {
               return (
-                <li className="flex flex-row flex-nowrap">
-                  <p>{tag}</p>
-                  <button onClick={(tag) => handleRemoveTag(tag)}>Remove</button>
-                </li>
+                <>
+                  <li key={tag} className="flex flex-row flex-nowrap justify-between my-5 mx-10 text-lg items-center">
+                    <p>{tag}</p>
+                    <button onClick={() => handleRemoveTag(tag)}>Remove</button>
+                  </li>
+                  <hr key={"hr" + tag} className="mx-10 opacity-30" />
+                </>
               );
             })}
           </ul>
-          <input
-            type="text"
-            placeholder="Tag Name"
-            name="tag-name"
-            onChange={(e) => setTagToAdd(e.target.value)}
-          ></input>
-          <button onClick={handleAddTag}>Add Tag</button>
+          <form className="flex flex-row w-full my-3" onSubmit={(e) => handleAddTag(e)}>
+            <input className="min-w-0 border grow mr-3 p-2" type="text" placeholder="Tag Name" name="tag_name"></input>
+            <button type="submit">Add Tag</button>
+          </form>
         </section>
       </div>
     </>
